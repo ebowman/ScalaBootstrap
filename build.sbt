82c2728d30f98da3a066dc3b77d8706a9b636d08
Eric Bowman <ebowman@gilt.com>
2012-01-06 14:55:15 +0000

    First working version

--- build.sbt
+++ build.sbt
@@ -3,3 +3,15 @@ name := "ScalaBootstrap"
 version := "0.0.1"
 
 scalaVersion := "2.9.1"
+
+resolvers ++= Seq(
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    "Akka Repository" at "http://akka.io/repository/"
+)
+
+libraryDependencies ++= Seq(
+    "com.typesafe.akka" % "akka-actor" % "2.0-M2",
+    "com.typesafe.akka" % "akka-remote" % "2.0-M2",
+    "com.typesafe.akka" % "akka-kernel" % "2.0-M2"
+)
+
---
+++ src/main/scala/SimpleAtomSearch.scala
@@ -0,0 +1,70 @@
+import akka.actor.{Actor, Props, ActorSystem}
+import java.util.concurrent.CountDownLatch
+import xml.XML
+
+
+object SimpleAtomSearch extends App {
+
+  val system = ActorSystem("SimpleAtomSearch")
+
+  val latch = new CountDownLatch(1)
+
+  val master = system.actorOf(Props(new Master(latch)))
+  println("Searching for %s" format args(0))
+  master ! Search(args(0))
+  latch.await()
+  system.shutdown()
+
+  def sanitize(text: String): String = {
+    text.replaceAll("""<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>""", " ").replaceAll("""[\s\n\r]+""", " ")
+  }
+}
+
+case class Entry(title: String, description: String, url: String)
+
+case class Search(query: String)
+
+case class SearchTask(url: String, query: String)
+
+case class Result(url: String, items: Traversable[Entry])
+
+class Master(latch: CountDownLatch) extends Actor {
+  val urls = Seq("https://api.gilt.com/v1/sales/men/active.atom",
+    "https://api.gilt.com/v1/sales/men/upcoming.atom",
+    "https://api.gilt.com/v1/sales/women/active.atom",
+    "https://api.gilt.com/v1/sales/women/upcoming.atom",
+    "https://api.gilt.com/v1/sales/kids/active.atom",
+    "https://api.gilt.com/v1/sales/kids/upcoming.atom",
+    "https://api.gilt.com/v1/sales/home/active.atom",
+    "https://api.gilt.com/v1/sales/home/upcoming.atom")
+  var pending = urls.size
+  val results = collection.mutable.Set[Entry]()
+
+  protected def receive = {
+    case Search(query) =>
+      urls foreach {
+        url => SimpleAtomSearch.system.actorOf(Props(new Slave)) ! SearchTask(url, query)
+      }
+    case Result(url, items) =>
+      pending -= 1
+      results ++= items
+      if (pending == 0) {
+        results.map(e => e.title + " -> " + e.url).toList.distinct.foreach(println)
+        latch.countDown()
+      }
+  }
+}
+
+class Slave extends Actor {
+  protected def receive = {
+    case SearchTask(url, query) =>
+      val atomXml = XML.load(url)
+      val result = for {
+        entryXml <- atomXml \\ "entry"
+        description = SimpleAtomSearch.sanitize((entryXml \ "content").text) if description.toLowerCase.contains(query)
+      } yield {
+        Entry((entryXml \ "title").text, description, (entryXml \\ "link").head.attribute("href").get.head.text)
+      }
+      sender ! Result(url, result)
+  }
+}
