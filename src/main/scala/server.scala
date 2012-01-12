f95a8f1da88ad9859d51cbf84dd2c7c27d98ce73
Adam <boniek@boniek-VirtualBox.(none)>
2012-01-12 14:53:08 +0000

    request actor

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,8 +1,11 @@
 package com.gilt.hackathon
 
 import java.io._
+import java.net._
+import akka.actor.Actor
 
 object Server extends App {
+
   var counter = 0;
   val ring = new Ring(find(new File("."), _.getName.endsWith(".xml")))
   var chunker = new Chunker(ring, 64)
@@ -24,6 +27,27 @@ object Server extends App {
   }
 }
 
+case class StartStream(socket: Socket, files: Iterable[File])
+case class NextChunk(output: OutputStream, socket: Socket, chunker: Chunker)
+
+class RequestActor() extends Actor {
+  val chunkSize = 1024
+  val header = "icy-metaint: " + chunkSize + "\n"
+  val meta = ""
+  
+  def receive = {
+    case StartStream(socket, files) =>
+      val output = socket.getOutputStream
+      output.write(header.getBytes)
+      self ! NextChunk(output, socket, new Chunker(files, chunkSize))
+    case NextChunk(output, socket, chunker) =>
+      val tuple = chunker.next
+      output.write(tuple._1)
+      output.write({0})
+      self ! NextChunk(output, socket, tuple._2)
+  }
+}
+
 case class Chunker(filesIter: Iterator[File], chunkSize: Int, file: Option[File], position: Int) {
   
   def this(files: Iterable[File], chunkSize: Int) = this(files.iterator, chunkSize, Option.empty, 0)
@@ -33,22 +57,22 @@ case class Chunker(filesIter: Iterator[File], chunkSize: Int, file: Option[File]
 
     val bytes = new Array[Byte](chunkSize)
     
+    val is = new BufferedInputStream(new FileInputStream(currentFile))
     try {
-      val is = new BufferedInputStream(new FileInputStream(currentFile))
       is.skip(position)
       val bytesRead = is.read(bytes, 0, chunkSize)
+
+      if (bytesRead < chunkSize) {
+        val myTuple = Chunker(filesIter, chunkSize - bytesRead, Option.empty, 0).next
+        val missingBytes = myTuple._1
+        val missingChunker = myTuple._2
+        Array.copy(missingBytes, 0, bytes, bytesRead, chunkSize - bytesRead)
+        (bytes, Chunker(filesIter, chunkSize, missingChunker.file, missingChunker.position))
+      } else {
+        (bytes, Chunker(filesIter, chunkSize, Option(currentFile), position + bytesRead))
+      }
     } finally {
       is.close();
-    }
-    
-    if (bytesRead < chunkSize) {
-      val myTuple = Chunker(filesIter, chunkSize - bytesRead, Option.empty, 0).next
-      val missingBytes = myTuple._1
-      val missingChunker = myTuple._2
-      Array.copy(missingBytes, 0, bytes, bytesRead, chunkSize - bytesRead)
-      (bytes, Chunker(filesIter, chunkSize, missingChunker.file, missingChunker.position))
-    } else {
-      (bytes, Chunker(filesIter, chunkSize, Option(currentFile), position + bytesRead))
     }
   }
   
