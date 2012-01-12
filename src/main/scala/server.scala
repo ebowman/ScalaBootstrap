712d3c1451b3e1ef7693beede1b590186c459605
Kevin O'Riordan <kevinorirl@kevinorirl-mpro.local>
2012-01-12 11:18:54 +0000

    First kevin branch checkin

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,6 +1,21 @@
 package com.gilt.hackathon
+import java.io.File
 
 object Server extends App {
-  println("hello, world")
+  val fileList = find(new File("/Users/kevinorirl/Music"), _.getName().endsWith(".mp3"))
+  println(fileList.toList.mkString("\n"))
+  
+  def find(dir:File, filterFunc:(File=>Boolean)):Iterable[File] = {
+    require(dir.exists() && dir.isDirectory())
+    def recurse(aDir : File):Iterable[File] = {
+    val inDir = dir.listFiles().filter(file => file.isFile() && filterFunc(file))
+    val subDirs = dir.listFiles().filter(_.isDirectory())
+    val subs = subDirs.flatMap(recurse)
+    inDir++subs
+  }
+    recurse(dir)
+  }
+  
+
 }
 
